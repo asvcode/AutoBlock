59998525fb25a441451b0defec18a46b0a624ebd
asvcode <25020209+asvcode@users.noreply.github.com>
2020-08-28 17:12:18 -0700

    updates

--- auto_block/core.py
+++ auto_block/core.py
@@ -5,6 +5,6 @@ __all__ = []
 # Cell
 import os
 
-from fastai2.basics import *
-#from fastai.callback.all import *
-from fastai2.vision.all import *
+from fastai.basics import *
+from fastai.callback.all import *
+from fastai.vision.all import *
